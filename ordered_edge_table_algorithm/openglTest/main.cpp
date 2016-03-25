8f36463d561bd9ad08c3f3ab0cca74cad8211dfa
PeterQi <376250533@qq.com>
2016-03-25 20:40:10 +0800

    sec

--- ordered_edge_table_algorithm/openglTest/main.cpp
+++ ordered_edge_table_algorithm/openglTest/main.cpp
@@ -1,11 +1,6 @@
 #include <GL/glut.h>
-
-void myDisplay(void)
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    glRectf(-0.5f, -0.5f, 0.5f, 0.5f);
-    glFlush();
-}
+#include <stdio.h>
+#define WINDOW_HEIGHT 500
 void renderScene(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -18,15 +13,86 @@ void renderScene(void)
 	glutSwapBuffers();
 }
 
+void myMouse(int button, int state, int x, int y)
+{
+
+	
+	if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON)
+	{
+		//while (x<1000 && y<1000) {
+		glBegin(GL_POINTS);
+		glVertex2i(x, WINDOW_HEIGHT - y);
+		printf("%d,%d\n", x, y);
+		//glVertex2i(x - 250, 250 - y);
+		//}
+
+		glEnd();
+		glFlush();
+	}
+	
+}
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPointSize(3);
+	glBegin(GL_POINTS);
+	glVertex2i(0, 0);
+	glVertex2i(250, 250);
+	glVertex2i(-250, -250);
+	glVertex2i(125, 125);
+	glEnd();
+	glFlush();
+}
+void Drawing()
+{
+	//glColor3f(1.0, 0.0, 0.0);
+	glBegin(GL_POINTS);
+	glVertex2i(200, 100);
+	glVertex2i(200, 300);
+	glEnd();
+
+	//glColor3f(0.0, 1.0, 0.0);
+	glBegin(GL_LINES);
+	glVertex2i(100, 100);
+	glVertex2i(300, 300);
+	glVertex2i(100, 300);
+	glVertex2i(300, 100);
+	glEnd();
+	glFlush();
+}
+
+void myDisplay(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	Drawing();
+	display();
+}
+void onReshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, w, 0, h);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+}
+void myIdle(void)
+{
+
+}
 int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);;
-    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-    glutInitWindowPosition(100, 100);
-    glutInitWindowSize(400, 400);
+    glutInitDisplayMode(GLUT_RGB);
+    glutInitWindowPosition(700, 400);
+    glutInitWindowSize(500, WINDOW_HEIGHT);
     glutCreateWindow("First GLUT Sample");
-    //glutDisplayFunc(&myDisplay);
-	glutDisplayFunc(renderScene);
+    glutDisplayFunc(&myDisplay);
+	//glutDisplayFunc(&renderScene);
+	glutMouseFunc(myMouse);
+	
+	glutReshapeFunc(onReshape);
     glutMainLoop();
     return 0;
 }
