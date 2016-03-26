603b5e281ac19e34b8d605984e1dbb65c0a93722
Peter <376250533@qq.com>
2016-03-26 12:23:30 +0800

    codig

--- ordered_edge_table_algorithm/openglTest/main.cpp
+++ ordered_edge_table_algorithm/openglTest/main.cpp
@@ -1,6 +1,14 @@
 #include <GL/glut.h>
 #include <stdio.h>
 #define WINDOW_HEIGHT 500
+#define SEGMENT_NUM 20
+
+segments seg[SEGMENT_NUM];
+int Ly_line[SEGMENT_NUM];
+int Hy_line[SEGMENT_NUM];
+int num = 0;
+int Last_x, Last_y;
+
 void renderScene(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -13,35 +21,89 @@ void renderScene(void)
 	glutSwapBuffers();
 }
 
+class segments
+{
+public:
+	double A, B, C;
+	int Ly, Hy;
+	segments(int x1, int x2, int y1, int y2)
+	{
+		A = y2 - y1;
+		B = x1 - x2;
+		C = x2*y1 - x1*y2;
+		Ly = (y1 < y2) ? y1 : y2;
+		Hy = (y1 > y2) ? y1 : y2;
+	}
+	int intersection(int y)
+	{
+		if (y > Hy || y < Ly)
+		{
+			return -1;
+		}
+		else if (Ly == Hy)
+		{
+			return -2;
+		}
+		else
+		{
+			double x = (-B*y - C) / A;
+			return (int)x;
+		}
+	}
+private:
+
+};
+
+void QuickSortLy(int start, int end)
+{
+	int i = start;
+	int j = end;
+	int key = seg[Ly_line[start]].Ly;
+	while (i < j)
+	{
+		while (seg[Ly_line[j]].Ly>key && i < j)j--;
+		Ly_line[i] = Ly_line[j];
+		while (seg[Ly_line[i]].Ly < key && i < j)i++;
+		Ly_line[j] = Ly_line[i];
+
+	}
+
+}
 void myMouse(int button, int state, int x, int y)
 {
-
-	
 	if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON)
 	{
-		//while (x<1000 && y<1000) {
-		glBegin(GL_POINTS);
 		glVertex2i(x, WINDOW_HEIGHT - y);
-		printf("%d,%d\n", x, y);
-		//glVertex2i(x - 250, 250 - y);
-		//}
+		if (num > 0)
+		{
+			seg[num](Last_x, Last_y, x, WINDOW_HEIGHT - y);
+		}
 
+		Last_x = x;
+		Last_y = WINDOW_HEIGHT - y;
+		num++;
+		printf("%d,%d\n", x, y);
+	}
+	else if (state == GLUT_DOWN && button == GLUT_RIGHT_BUTTON)
+	{
 		glEnd();
 		glFlush();
+
+		num = 0;
+
+		for (int i = 0; i < SEGMENT_NUM; i++)
+		{
+			Ly_line[i] = i;
+			Hy_line[i] = i;
+		}
+
 	}
-	
 }
+
 void display()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glPointSize(3);
-	glBegin(GL_POINTS);
-	glVertex2i(0, 0);
-	glVertex2i(250, 250);
-	glVertex2i(-250, -250);
-	glVertex2i(125, 125);
-	glEnd();
-	glFlush();
+	//glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_LINE_LOOP);
 }
 void Drawing()
 {
@@ -63,10 +125,12 @@ void Drawing()
 
 void myDisplay(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	Drawing();
+	//glClear(GL_COLOR_BUFFER_BIT);
 	display();
+	//Drawing();
 }
+
+
 void onReshape(int w, int h)
 {
 	glViewport(0, 0, w, h);
@@ -77,15 +141,18 @@ void onReshape(int w, int h)
 	glLoadIdentity();
 
 }
-void myIdle(void)
-{
 
-}
+
 int main(int argc, char *argv[])
 {
+	for (int i = 0; i < SEGMENT_NUM; i++)
+	{
+		Ly_line[i] = i;
+		Hy_line[i] = i;
+	}
     glutInit(&argc, argv);;
     glutInitDisplayMode(GLUT_RGB);
-    glutInitWindowPosition(700, 400);
+    glutInitWindowPosition(500, 200);
     glutInitWindowSize(500, WINDOW_HEIGHT);
     glutCreateWindow("First GLUT Sample");
     glutDisplayFunc(&myDisplay);
